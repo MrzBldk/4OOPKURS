81250ca72fefa8649b2f97ed8a4f116db54095ee
Mrz Bldk <anton_lityagin@mail.ru>
2021-05-13 17:10:06 +0300

    Let the party begin

--- KURS/KURS/AppShell.xaml.cs
+++ KURS/KURS/AppShell.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using KURS.Views;
 
 namespace KURS
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : Shell
     {
-        public MainPage()
+        public AppShell()
         {
             InitializeComponent();
         }
